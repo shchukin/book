fd660248b31174a6c582378b4b557afbd2f4c378
Anton Shchukin <a.a.shchukin@gmail.com>
2024-06-01 18:23:08 +0700

    fonts and other related global files

---
+++ src/components/Lesson/Lesson.tsx
@@ -0,0 +1,12 @@
+import './Lesson.css'
+
+function Lesson({children}) {
+
+  return (
+    <div className="lesson">
+      {children}
+    </div>
+  )
+}
+
+export default Lesson;
---
+++ src/components/Thai/Thai.tsx
@@ -0,0 +1,12 @@
+import './Thai.css'
+
+function Thai({children}) {
+
+  return (
+    <span className="thai">
+      {children}
+    </span>
+  )
+}
+
+export default Thai;
