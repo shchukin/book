a97f964ab4ce7e121dca4bd0cad77ecd6191aa80
Anton Shchukin <a.a.shchukin@gmail.com>
2024-06-13 00:54:26 +0700

    better mulmultiple highlight

--- src/components/Question/Question.tsx
+++ src/components/Question/Question.tsx
@@ -12,18 +12,31 @@ type QuestionProps = {
   maxLength?: number;
 };
 
+type OptionStatus = {
+  value: string;
+  isCorrect: boolean | null;
+};
+
 const Question: React.FC<QuestionProps> = ({ question, type, correctAnswer, options, maxLength }) => {
-  const [selectedOption, setSelectedOption] = useState<string>('');
+  const [selectedOptions, setSelectedOptions] = useState<OptionStatus[]>(
+    options?.map(option => ({ value: option, isCorrect: null })) || []
+  );
+  const [selectedOption, setSelectedOption] = useState<string | null>(null);
   const [userAnswer, setUserAnswer] = useState<string>('');
   const [showAnswer, setShowAnswer] = useState(false);
-  const [isCorrect, setIsCorrect] = useState<boolean | null>(null);
 
   const radioName = generateRandomString(16); // Generate a random string for the radio button name
 
   const handleOptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const selectedValue = event.target.value;
     setSelectedOption(selectedValue);
-    setIsCorrect(selectedValue === correctAnswer);
+    setSelectedOptions(prevOptions =>
+      prevOptions.map(option =>
+        option.value === selectedValue
+          ? { ...option, isCorrect: selectedValue === correctAnswer }
+          : option
+      )
+    );
   };
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -43,16 +56,19 @@ const Question: React.FC<QuestionProps> = ({ question, type, correctAnswer, opti
       {type === 'radio' && (
         <>
           <Big><Thai>{question}</Thai></Big>
-          {options && options.map((option, optionIndex) => (
-            <label key={optionIndex} className={`radio-label ${selectedOption === option ? (isCorrect ? 'correct' : 'incorrect') : ''}`}>
+          {selectedOptions.map((option, optionIndex) => (
+            <label
+              key={optionIndex}
+              className={`radio-label ${option.isCorrect === null ? '' : option.isCorrect ? 'correct' : 'incorrect'}`}
+            >
               <input
                 type="radio"
                 name={`question-${radioName}`} // Use the generated random string for the name
-                value={option}
-                checked={selectedOption === option}
+                value={option.value}
+                checked={selectedOption === option.value}
                 onChange={handleOptionChange}
               />
-              {option}
+              {option.value}
             </label>
           ))}
           <div className="question__answer">
@@ -67,13 +83,13 @@ const Question: React.FC<QuestionProps> = ({ question, type, correctAnswer, opti
             <Big><Thai>{question}</Thai> –&nbsp;</Big>
           </div>
           <input
-            className={`question__text-input${isCorrect !== null && isCorrect ? ' question__text-input--success' : ''}${isCorrect !== null && !isCorrect ? ' question__text-input--error' : ''}`}
+            className={`question__text-input${userAnswer === correctAnswer ? ' question__text-input--success' : userAnswer && ' question__text-input--error'}`}
             type="text"
             value={userAnswer}
             onChange={handleInputChange}
             placeholder="???"
           />
-          <button className="question__check-handler" onClick={() => setIsCorrect(userAnswer === correctAnswer)}>Проверить</button>
+          <button className="question__check-handler" onClick={() => setUserAnswer(userAnswer)}>Проверить</button>
           <div className="question__answer">
             {!showAnswer && <button className="question__show-answer" onClick={toggleAnswer}>Показать ответ</button>}
             {showAnswer && <div><span className="question__answer-label">Ответ:</span> <Big><Thai>{correctAnswer}</Thai></Big></div>}
@@ -83,7 +99,7 @@ const Question: React.FC<QuestionProps> = ({ question, type, correctAnswer, opti
       {type === 'typing' && (
         <div className="question__asking">
           <input
-            className={`question__text-input${isCorrect !== null && isCorrect ? ' question__text-input--success' : ''}${isCorrect !== null && !isCorrect ? ' question__text-input--error' : ''}`}
+            className={`question__text-input${userAnswer === correctAnswer ? ' question__text-input--success' : userAnswer && ' question__text-input--error'}`}
             type="text"
             value={userAnswer}
             onChange={handleInputChange}
