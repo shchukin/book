b3f879b45b2feabf5d219f2ff6c14d90e32a352f
Anton Shchukin <a.a.shchukin@gmail.com>
2024-06-15 14:29:56 +0700

    remove autoprefixer, simplify postcssPresentEmv

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,11 +1,5 @@
 module.exports = {
     plugins: {
-        autoprefixer: {},
-        'postcss-preset-env': {
-            stage: 1, // Adjust the stage according to your needs
-            features: {
-                'nesting-rules': true, // Example feature
-            },
-        },
+        'postcss-preset-env': {}
     },
 };
