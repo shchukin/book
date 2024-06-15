9b3ad1dd57d376084875724f038dcfd9b92ee8f3
Anton Shchukin <a.a.shchukin@gmail.com>
2024-06-15 18:11:54 +0700

    ex  17 with translation

--- src/components/Exercise1x17/Exercise1x17.tsx
+++ src/components/Exercise1x17/Exercise1x17.tsx
@@ -1,26 +1,43 @@
-import {Exercise, ExerciseEntry, ExerciseHead} from "../Exercise/Exercise.tsx";
+import React from 'react';
+import { Exercise, ExerciseEntry, ExerciseHead } from "../Exercise/Exercise.tsx";
+import Texting from '../Texting/Texting';
+
+const questions = [
+  { question: "Дедушка спрашивает бабушку.", correctAnswer: "ตาถามยาย" },
+  { question: "Бабушка спрашивает сторожа.", correctAnswer: "ยายถามยาม" },
+  { question: "Малышка просит бабушку.", correctAnswer: "หนูขอยาย" },
+  { question: "Са просит дедушку.", correctAnswer: "สาขอตา" },
+  { question: "Бабушка просит подушку.", correctAnswer: "ยายขอหมอน" },
+  { question: "Сторож просит краску.", correctAnswer: "ยามขอสี" },
+  { question: "Са учит малышку.", correctAnswer: "สาสอนหนู" },
+  { question: "Дедушка учит сторожа.", correctAnswer: "ตาสอนยาม" },
+  { question: "Бабушка взбивает подушку.", correctAnswer: "ยายตีหมอน" },
+  { question: "Бабушка намазала шею.", correctAnswer: "ยายนวดคอ" },
+  { question: "Сторож втирает лекарство.", correctAnswer: "ยามทายา" },
+  { question: "Дедушка красит.", correctAnswer: "ตาทาสี" },
+  { question: "Дедушка продает свинину.", correctAnswer: "ตาขายหมู" },
+  { question: "У бабушки есть краски.", correctAnswer: "ยายมีสี" },
+  { question: "У Са есть лекарство.", correctAnswer: "สามียา" },
+  { question: "Бабушка спит.", correctAnswer: "ยายนอน" },
+  { question: "Дедушка мерзнет.", correctAnswer: "ตาหนาว" },
+];
 
 function Exercise1x17() {
-
   return (
     <Exercise>
       <ExerciseHead>
-        17. Переведите
+        17. Переведите, запишите ответ по-тайски
       </ExerciseHead>
-      <ExerciseEntry>
-          Дедушка спрашивает бабушку. Бабушка спрашивает сторожа.
-          Малышка просит бабушку. Са просит дедушку.
-          Бабушка просит подушку. Сторож просит краску.
-          Са учит малышку. Дедушка учит сторожа.
-          Бабушка взбивает подушку.
-          Бабушка намазала шею.
-          Сторож втирает лекарство. Дедушка красит.
-          Дедушка продает свинину.
-          У бабушки есть краски. У Са есть лекарство.
-          Бабушка спит. Дедушка мерзнет.
-      </ExerciseEntry>
+      {questions.map((item, index) => (
+        <ExerciseEntry key={index}>
+          <Texting
+            question={item.question}
+            correctAnswer={item.correctAnswer}
+          />
+        </ExerciseEntry>
+      ))}
     </Exercise>
-  )
+  );
 }
 
 export default Exercise1x17;
