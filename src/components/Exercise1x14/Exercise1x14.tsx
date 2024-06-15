6af842e396bee3b4837ffe682c93b3f9b0c415ad
Anton Shchukin <a.a.shchukin@gmail.com>
2024-06-15 17:43:41 +0700

    ex14

--- src/components/Exercise1x14/Exercise1x14.tsx
+++ src/components/Exercise1x14/Exercise1x14.tsx
@@ -5,18 +5,18 @@ import Texting from "../Texting/Texting.tsx";
 function Exercise1x14() {
 
   const questionsAndAnswers = [
-    { question: 'ха:й', answer: 'หาย', audio: 'Lesson01xEx12-01-thaam.mp3' },
-    { question: 'пху:', answer: 'พู', audio: 'Lesson01xEx12-01-thaam.mp3' },
-    { question: 'йɔ:м', answer: 'ยอม', audio: 'Lesson01xEx12-01-thaam.mp3' },
-    { question: 'ни:', answer: 'หนี', audio: 'Lesson01xEx12-01-thaam.mp3' },
-    { question: 'са:м', answer: 'สาม', audio: 'Lesson01xEx12-01-thaam.mp3' },
-    { question: 'ма:й', answer: 'หมาย', audio: 'Lesson01xEx12-01-thaam.mp3' },
-    { question: 'пхи:', answer: 'ผี', audio: 'Lesson01xEx12-01-thaam.mp3' },
-    { question: 'ха:у', answer: 'หาว', audio: 'Lesson01xEx12-01-thaam.mp3' },
-    { question: 'пха:й', answer: 'พาย', audio: 'Lesson01xEx12-01-thaam.mp3' },
-    { question: 'на:н', answer: 'นาน', audio: 'Lesson01xEx12-01-thaam.mp3' },
-    { question: 'ну:', answer: 'หนู', audio: 'Lesson01xEx12-01-thaam.mp3' },
-    { question: 'ма:', answer: 'มา', audio: 'Lesson01xEx12-01-thaam.mp3' },
+    { question: 'ха:й', answer: 'หาย', audio: 'Lesson01xEx14-01-haay.mp3' },
+    { question: 'пху:', answer: 'พู', audio: 'Lesson01xEx14-02-phuu.mp3' },
+    { question: 'йɔ:м', answer: 'ยอม', audio: 'Lesson01xEx14-03-yoom.mp3' },
+    { question: 'ни:', answer: 'หนี', audio: 'Lesson01xEx14-04-nii.mp3' },
+    { question: 'са:м', answer: 'สาม', audio: 'Lesson01xEx14-05-saam.mp3' },
+    { question: 'ма:й', answer: 'หมาย', audio: 'Lesson01xEx14-06-maay.mp3' },
+    { question: 'пхи:', answer: 'ผี', audio: 'Lesson01xEx14-07-phii.mp3' },
+    { question: 'ха:у', answer: 'หาว', audio: 'Lesson01xEx14-08-haay.mp3' },
+    { question: 'пха:й', answer: 'พาย', audio: 'Lesson01xEx14-09-phaay.mp3' },
+    { question: 'на:н', answer: 'นาน', audio: 'Lesson01xEx14-10-naan.mp3' },
+    { question: 'ну:', answer: 'หนู', audio: 'Lesson01xEx14-11-nuu.mp3' },
+    { question: 'ма:', answer: 'มา', audio: 'Lesson01xEx14-12-maa.mp3' },
   ];
 
   return (
