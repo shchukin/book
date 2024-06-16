d926b9c6dfcad80d0061ec8933c3c29622e86ded
Anton Shchukin <a.a.shchukin@gmail.com>
2024-06-16 12:35:47 +0700

    ex15

--- src/components/Exercise1x15/Exercise1x15.tsx
+++ src/components/Exercise1x15/Exercise1x15.tsx
@@ -2,26 +2,32 @@ import {Exercise, ExerciseEntry, ExerciseHead} from "../Exercise/Exercise.tsx";
 import Sparse, {SparseItem} from "../Sparse/Sparse.tsx";
 import Thai from "../Thai/Thai.tsx";
 
+const data = [
+  "…ถามสา",
+  "ยายถาม…",
+  "…สอนสา",
+  "ยายสอน…",
+  "…ขอตา",
+  "ยายขอ…",
+  "…ขอสี",
+  "สาขอ…",
+  "…มียา",
+  "ยายมี…"
+];
+
 function Exercise1x15() {
 
   return (
     <Exercise>
       <ExerciseHead>
-        15. Подставьте подходящие по смыслу слова из тех, которые вы уже знаете.
+        15. Подставьте подходящие по смыслу слова из тех, которые вы уже знаете
       </ExerciseHead>
       <ExerciseEntry>
         <Thai size="big">
           <Sparse>
-            <SparseItem>…ถามสา</SparseItem>
-            <SparseItem>ยายถาม…</SparseItem>
-            <SparseItem>…สอนสา</SparseItem>
-            <SparseItem>ยายสอน…</SparseItem>
-            <SparseItem>…ขอตา</SparseItem>
-            <SparseItem>ยายขอ…</SparseItem>
-            <SparseItem>…ขอสี</SparseItem>
-            <SparseItem>สาขอ…</SparseItem>
-            <SparseItem>…มียา</SparseItem>
-            <SparseItem>ยายมี…</SparseItem>
+            {data.map((item, index) => (
+              <SparseItem key={index}>{item}</SparseItem>
+            ))}
           </Sparse>
         </Thai>
       </ExerciseEntry>
